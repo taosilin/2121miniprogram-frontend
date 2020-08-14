d5548fe7ee266068e45f64def1f7fc70bd0f2fdd
taosilin <852321819@qq.com>
2020-08-14 16:49:37 +0800

    个人中心页面

--- pages/userCenter/userCenter.js
+++ pages/userCenter/userCenter.js
@@ -1,4 +1,5 @@
 // pages/userCenter/userCenter.js
+const app = getApp()
 Page({
 
   /**
@@ -12,7 +13,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({userInfo:app.globalData.userInfo})
+    // console.log(this.data.userInfo)
   },
 
   /**
