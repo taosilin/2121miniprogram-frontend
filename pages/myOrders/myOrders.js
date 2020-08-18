7369603200fc85365bb4b5ec713b09e6d02cf53e
taosilin <852321819@qq.com>
2020-08-18 17:18:12 +0800

    添加了无订单状态

--- pages/myOrders/myOrders.js
+++ pages/myOrders/myOrders.js
@@ -5,133 +5,170 @@ Page({
    * 页面的初始数据
    */
   data: {
-    orders:[
+    // orders:[
+    //   {
+    //     orderdate:"2020-08-01",
+    //     orderid:"12345567788898",
+    //     state:"待付款",
+    //     sum:550,
+    //     commodityList:[
+    //       {
+    //         name:"银色细边眼镜",
+    //         cmdid:"xxxxxx1",
+    //         color:"银色",
+    //         price:275,
+    //         num:1,
+    //         leftdegree:-3.25,
+    //         rightdegree:-3.75,
+    //         picture:"../../image/glasses.jpg"
+    //       },
+    //       {
+    //         name:"银色细边眼镜",
+    //         cmdid:"xxxxxx2",
+    //         color:"银色",
+    //         price:275,
+    //         num:1,
+    //         leftdegree:-3.25,
+    //         rightdegree:-3.75,
+    //         picture:"../../image/glasses.jpg"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     orderdate:"2020-08-01",
+    //     orderid:"12345567788898",     
+    //     state:"待付款",
+    //     sum:299,
+    //     commodityList:[
+    //       {
+    //         name:"黑色粗边眼镜",
+    //         cmdid:"1234567890",
+    //         color:"黑色",
+    //         price:299,
+    //         num:1,
+    //         leftdegree:-5.50,
+    //         rightdegree:-5.50,
+    //         picture:"../../image/glasses.jpg",
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     orderdate:"2020-08-01",
+    //     orderid:"12345567788898",
+    //     sum:299,
+    //     state:"待付款",
+    //     commodityList:[
+    //       {
+    //         name:"黑色粗边眼镜",
+    //         cmdid:"1234567890",
+    //         color:"黑色",
+    //         price:299,
+    //         num:1,
+    //         leftdegree:-5.50,
+    //         rightdegree:-5.50,
+    //         picture:"../../image/glasses.jpg",
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     orderdate:"2020-08-17",
+    //     orderid:"12345567788898",
+    //     sum:299,
+    //     state:"待收货",
+    //     commodityList:[
+    //       {
+    //         name:"黑色粗边眼镜",
+    //         cmdid:"1234567890",
+    //         color:"黑色",
+    //         price:299,
+    //         num:1,
+    //         leftdegree:-5.50,
+    //         rightdegree:-5.50,
+    //         picture:"../../image/glasses.jpg",
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     orderdate:"2020-08-17",
+    //     orderid:"123456789098",
+    //     sum:299,
+    //     state:"待评价",
+    //     commodityList:[
+    //       {
+    //         name:"黑色粗边眼镜",
+    //         cmdid:"1234567890",
+    //         color:"黑色",
+    //         price:299,
+    //         num:1,
+    //         leftdegree:-5.50,
+    //         rightdegree:-5.50,
+    //         picture:"../../image/glasses.jpg",
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     orderdate:"2020-08-17",
+    //     orderid:"12345567788898",
+    //     sum:599,
+    //     state:"已完成",
+    //     commodityList:[
+    //       {
+    //         name:"黑色粗边眼镜",
+    //         cmdid:"1234567890",
+    //         color:"黑色",
+    //         price:599,
+    //         num:1,
+    //         leftdegree:-5.50,
+    //         rightdegree:-5.50,
+    //         picture:"../../image/glasses.jpg",
+    //       }
+    //     ]
+    //   }
+    // ],
+    orders:[],
+    recommend:[
       {
-        orderdate:"2020-08-01",
-        orderid:"12345567788898",
-        state:"待付款",
-        sum:550,
-        commodityList:[
-          {
-            name:"银色细边眼镜",
-            cmdid:"xxxxxx1",
-            color:"银色",
-            price:275,
-            num:1,
-            leftdegree:-3.25,
-            rightdegree:-3.75,
-            picture:"../../image/glasses.jpg"
-          },
-          {
-            name:"银色细边眼镜",
-            cmdid:"xxxxxx2",
-            color:"银色",
-            price:275,
-            num:1,
-            leftdegree:-3.25,
-            rightdegree:-3.75,
-            picture:"../../image/glasses.jpg"
-          }
-        ]
+        name:"银色细边眼镜",
+        price:275,
+        imgUrl:'../../image/glasses.jpg'
       },
       {
-        orderdate:"2020-08-01",
-        orderid:"12345567788898",     
-        state:"待付款",
-        sum:299,
-        commodityList:[
-          {
-            name:"黑色粗边眼镜",
-            cmdid:"1234567890",
-            color:"黑色",
-            price:299,
-            num:1,
-            leftdegree:-5.50,
-            rightdegree:-5.50,
-            picture:"../../image/glasses.jpg",
-          }
-        ]
+        name:"银色细边眼镜",
+        price:599,
+        imgUrl:'../../image/glasses.jpg'
       },
       {
-        orderdate:"2020-08-01",
-        orderid:"12345567788898",
-        sum:299,
-        state:"待付款",
-        commodityList:[
-          {
-            name:"黑色粗边眼镜",
-            cmdid:"1234567890",
-            color:"黑色",
-            price:299,
-            num:1,
-            leftdegree:-5.50,
-            rightdegree:-5.50,
-            picture:"../../image/glasses.jpg",
-          }
-        ]
+        name:"银色细边眼镜",
+        price:299,
+        imgUrl:'../../image/glasses.jpg'
       },
       {
-        orderdate:"2020-08-17",
-        orderid:"12345567788898",
-        sum:299,
-        state:"待收货",
-        commodityList:[
-          {
-            name:"黑色粗边眼镜",
-            cmdid:"1234567890",
-            color:"黑色",
-            price:299,
-            num:1,
-            leftdegree:-5.50,
-            rightdegree:-5.50,
-            picture:"../../image/glasses.jpg",
-          }
-        ]
+        name:"银色细边眼镜",
+        price:275,
+        imgUrl:'../../image/glasses.jpg'
       },
       {
-        orderdate:"2020-08-17",
-        orderid:"123456789098",
-        sum:299,
-        state:"待评价",
-        commodityList:[
-          {
-            name:"黑色粗边眼镜",
-            cmdid:"1234567890",
-            color:"黑色",
-            price:299,
-            num:1,
-            leftdegree:-5.50,
-            rightdegree:-5.50,
-            picture:"../../image/glasses.jpg",
-          }
-        ]
+        name:"银色细边眼镜",
+        price:275,
+        imgUrl:'../../image/glasses.jpg'
       },
       {
-        orderdate:"2020-08-17",
-        orderid:"12345567788898",
-        sum:599,
-        state:"已完成",
-        commodityList:[
-          {
-            name:"黑色粗边眼镜",
-            cmdid:"1234567890",
-            color:"黑色",
-            price:599,
-            num:1,
-            leftdegree:-5.50,
-            rightdegree:-5.50,
-            picture:"../../image/glasses.jpg",
-          }
-        ]
+        name:"银色细边眼镜",
+        price:275,
+        imgUrl:'../../image/glasses.jpg'
       }
-    ]
+    ],
+    windowHeight:700
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const sysInfo = wx.getSystemInfoSync();
+    this.setData({
+      windowHeight:sysInfo.windowHeight-32
+    })
   },
 
   /**
