193fab5429d62fc19113e75329914dab35be5aba
taosilin <852321819@qq.com>
2020-08-21 18:22:04 +0800

    菜单组件

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -7,6 +7,22 @@ Component({
     menuName:{
       type:String,
       value:'菜单'
+    },
+    menuItem:{
+      type:Array,
+      value:['item1','item2','item3']
+    },
+    width:{
+      type:String,
+      value:'100'
+    },
+    height:{
+      type:String,
+      value:'30'
+    },
+    titleSize:{
+      type:String,
+      value:'14'
     }
   },
 
@@ -14,13 +30,28 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    menuOpen:false,
+    selectId:0,
+    selectName:''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    openMenu:function(){
+      this.setData({
+        menuOpen:!this.data.menuOpen
+      })
+    },
+    selectItem:function(e){
+      var selectId=e.target.dataset.id;
+      var selectName=e.target.dataset.model;
+      console.log(selectId,selectName)
+      this.setData({
+        selectId:selectId,
+        selectName:selectName
+      })
+    }
   }
 })
