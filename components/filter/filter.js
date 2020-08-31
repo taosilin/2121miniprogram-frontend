6683c047c0cee744ccba3aad9f5e34c629d87b9a
taosilin <852321819@qq.com>
2020-08-31 15:19:16 +0800

    filter筛选组件
    filter

--- components/filter/filter.js
+++ components/filter/filter.js
@@ -1,4 +1,4 @@
-// components/fliter/fliter.js
+// components/filter/filter.js
 Component({
   /**
    * 组件的属性列表
@@ -16,26 +16,26 @@ Component({
       type:String,
       value:'14'
     },
-    fliterList:{
+    filterList:{
       type:Array,
       value:[
         {
-          fliterName:"折射率",
+          filterName:"折射率",
           options:[1.56,1.60,1.67,1.74],
           selectItem:0
         },
         {
-          fliterName:"镜片功能",
+          filterName:"镜片功能",
           options:["高清绿膜镜片","感光变色镜片","双效抗疲劳镜片","科技膜层变色镜片"],
           selectItem:0
         },
         {
-          fliterName:"镜片材质",
+          filterName:"镜片材质",
           options:["PC","树脂"],
           selectItem:0
         },
         {
-          fliterName:"适用对象",
+          filterName:"适用对象",
           options:["中年","老年","少年","青年"],
           selectItem:0
         }
@@ -47,29 +47,30 @@ Component({
    * 组件的初始数据
    */
   data: {
-    fliterOpen:false
+    filterOpen:false
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    openFliter:function(){
+    openFilter:function(){
       this.setData({
-        fliterOpen:!this.data.fliterOpen
+        filterOpen:!this.data.filterOpen
       })
     },
-    fliterChange:function(e){
+    filterChange:function(e){
       let i = e.currentTarget.dataset.i;
       let j = e.target.dataset.j;
       console.log(i,j);
       if (j!=undefined){
-        let items = this.data.fliterList;
+        let items = this.data.filterList;
         items[i].selectItem = j;
         this.setData({
-          fliterList:items
+          filterList:items
         })
       }
     }
   }
 })
+
